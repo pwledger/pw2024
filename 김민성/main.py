e3c839d3da3286e2b226b72e0158e2bab56f50ce
김민성 <superminsung7475@gmail.com>
2024-01-10 17:36:04 +0900

    김민성 완성

--- 김민성/main.py
+++ 김민성/main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
@@ -6,7 +7,8 @@ capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 while cv2.waitKey(33) < 0:
     ret, frame = capture.read()
-    cv2.imshow("VideoFrame", frame)
+    frame = cv2.rectangle(frame, (200, 200), (50, 50), (255, 255, 255), 5, cv2.LINE_8)
+    cv2.imshow(frame)
 
 capture.release()
 cv2.destroyAllWindows()
