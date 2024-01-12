45fd0720d2237b6f01eb345650c72b68ee6f4ce4
김민성 <superminsung7475@gmail.com>
2024-01-12 17:59:56 +0900

    main.py 수정

--- 정지호/main.py
+++ 정지호/main.py
@@ -4,7 +4,7 @@ capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-while cv2.waitKey(33) < 0:
+while cv2.waitKey(33) !=  ord('q'):
     ret, frame = capture.read()
     cv2.imshow("VideoFrame", frame)
 
